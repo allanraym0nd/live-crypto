36c86b4b9f23f66ef0e6f60aab4ae73d737f5ccc
allan raymond <raymonda916@gmail.com>
2025-05-09 10:08:31 +0300

    used fetch instead of jquery

--- index.js
+++ index.js
@@ -4,20 +4,36 @@ var eth =document.getElementById('ethereum');
 var doge =document.getElementById('dogecoin');
 
 
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum,dogecoin",
-    "method": "GET",
-    "headers": {
-        "accept": "application/json",
+// var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum,dogecoin",
+//     "method": "GET",
+//     "headers": {
+//         "accept": "application/json",
         
      
-    }
-  };
+//     }
+//   };
 
-  $.ajax(settings).done(function (response) {
-      btc.innerHTML =response.bitcoin.usd
-      eth.innerHTML =response.ethereum.usd
-      doge.innerHTML =response.dogecoin.usd
-  });
+//   $.ajax(settings).done(function (response) {
+//       btc.innerHTML =response.bitcoin.usd
+//       eth.innerHTML =response.ethereum.usd
+//       doge.innerHTML =response.dogecoin.usd
+//   });
+
+
+
+fetch ("https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum,dogecoin")
+.then(response => response.json())
+.then(data => {
+     btc.innerHTML= data.bitcoin.usd;
+     eth.innerHTML= data.ethereum.usd;
+     doge.innerHTML= data.dogecoin.usd;
+    
+
+})
+.catch(error => {
+    console.error("Error fetching crypto prices:", error);
+});
+
